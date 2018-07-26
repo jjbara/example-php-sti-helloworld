aecd956f8f490c6a6bef3e33107f9c5d0fd6db1f
jjbara <41361619+jjbara@users.noreply.github.com>
2018-07-26 15:40:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 		if (isset($_POST["click"])) 
 		{
 			$res=shell_exec("wget -O - https://rawgit.com/jjbara/example-php-sti-helloworld/master/test.sh | bash");
-			echo("test");
+			echo($res);
 		}
 		else
 		{
